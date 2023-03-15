c9ce535bc5f7e56b16cdd214f89fc9f6f666c531
Shital <shitalmoule1234@gmail.com>
2023-03-15 21:18:51 +0530

    First Commit

---
+++ SpringIOC/src/main/java/org/tnsif/springioc/Jio.java
@@ -0,0 +1,12 @@
+package org.tnsif.springioc;
+
+public class Jio implements Sim{
+
+	public void call() {
+		System.out.println("Call using jio");
+	}
+	
+	public void message() {
+		System.out.println("Reliance Jio powered by - Ambani");
+	}
+}
---
+++ SpringIOC/src/main/java/org/tnsif/springioc/Sim.java
@@ -0,0 +1,8 @@
+package org.tnsif.springioc;
+
+public interface Sim {
+
+	//by default it is abstract 
+		void call();
+		void message();
+}
